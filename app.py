36bb8aff76df5dfd2bd18197a3d1d515f4e72912
Sanaz <sanaazmhi@gmail.com>
2025-05-16 15:26:56 +0200

    update requirments filee and test backend and front end

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) 
 
 @app.route('/api/hello')
 def hello():
-    return 'Hello Sanaz,Welcom to your new Docker Flask App!'
+    return jsonify({'message': 'Hello Sanaz from Flask!'})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
+
